1d5e0ae410d4eb224fe25044412199cfd17d21db
Roubaty Anthony <anthony.roubaty@gmail.com>
2014-10-27 14:43:05 +0100

    Version qui marche !!

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('twebProject1App')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
