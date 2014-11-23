dc6c947b678244b67a1aba2745510f51079214cd
Auriana <auriana.hug@heig-vd.ch>
2014-11-23 13:20:43 +0100

    updating chat

--- client/app/testPdf/testPdf.controller.js
+++ client/app/testPdf/testPdf.controller.js
@@ -1,84 +1,62 @@
 'use strict';
-
 angular.module('twebProject1App')
 .controller('TestpdfCtrl', function ($scope, $http, socket) {
 	//presentation title
 	$scope.titlePresentation = "Test Presentation";
-
 	// Chat part
 	$scope.listeMsg = [];
 	$scope.date = new Date();
-	
 	$http.get('/api/messages').success(function(listeMsg) {
 		$scope.listeMsg = listeMsg;
 		socket.syncUpdates('message', $scope.listeMsg);
-    });
-
+	});
 	$scope.send = function() {
-      if($scope.inputChat === '') {
-        return;
-      }
-	  
-	  $scope.date = new Date();
-	  $scope.formedDate = $scope.date.getHours() + 'h' + $scope.date.getMinutes() + 'm' + $scope.date.getSeconds() + 's';
-	  
-      $http.post('/api/messages', { name: $scope.inputChat , time : $scope.formedDate, type : "message"});
-      $scope.inputChat = '';
-	  
-    };
-	
+		if($scope.inputChat === '') {
+			return;
+		}
+		$scope.date = new Date();
+		$scope.formedDate = $scope.date.getHours() + 'h' + $scope.date.getMinutes() + 'm' + $scope.date.getSeconds() + 's';
+		$http.post('/api/messages', { name: $scope.inputChat , time : $scope.formedDate, type : "message"});
+		$scope.inputChat = '';
+	};
 	$scope.$on('$destroy', function () {
-      socket.unsyncUpdates('message');
-    });	
-	
+		socket.unsyncUpdates('message');
+	});
 	$scope.scroll = function(){
 		document.getElementById('chatDisplay').scrollTop = 99999;
 	}
-
 	//Button
 	$scope.slowBut = function(){
 		$scope.date = new Date();
 		$scope.formedDate = $scope.date.getHours() + 'h' + $scope.date.getMinutes() + 'm' + $scope.date.getSeconds() + 's';
-	  
 		$http.post('/api/messages', { name: "Slow down!" , time : $scope.formedDate, type : "slow"});
 	}
-	
 	$scope.loudBut = function(){
 		$scope.date = new Date();
 		$scope.formedDate = $scope.date.getHours() + 'h' + $scope.date.getMinutes() + 'm' + $scope.date.getSeconds() + 's';
-	  
 		$http.post('/api/messages', { name: "Louder, please!" , time : $scope.formedDate, type : "loud"});
 	}
-	
 	$scope.lostBut = function(){
 		$scope.date = new Date();
 		$scope.formedDate = $scope.date.getHours() + 'h' + $scope.date.getMinutes() + 'm' + $scope.date.getSeconds() + 's';
-	  
 		$http.post('/api/messages', { name: "I'm lost !" , time : $scope.formedDate, type : "lost"});
 	}
-	
 	$scope.inteBut = function(){
 		$scope.date = new Date();
 		$scope.formedDate = $scope.date.getHours() + 'h' + $scope.date.getMinutes() + 'm' + $scope.date.getSeconds() + 's';
-	  
 		$http.post('/api/messages', { name: "Interesting..." , time : $scope.formedDate, type : "interesting"});
 	}
-	
 	//synchronisation des pages du PDF
 	socket.socket.on('pageNumber', function(num) {
-          queueRenderPage(num);
-          pageNum = num;
+		queueRenderPage(num);
+		pageNum = num;
 	});
-	
 	//////////////////////
 	//
 	// If absolute URL from the remote server is provided, configure the CORS
 	// header on that server.
 	//
-
-	var url = './data/WebInfra.pdf';	
-
-
+	var url = './data/WebInfra.pdf';
 	//
 	// Fetch the PDF document from the URL using promises
 	//
@@ -104,7 +82,6 @@ angular.module('twebProject1App')
 			page.render(renderContext);
 		});
 	});
-
 	var pdfDoc = null,
 		pageNum = 1,
 		pageRendering = false,
@@ -112,11 +89,10 @@ angular.module('twebProject1App')
 		scale = 0.8,
 		canvas = document.getElementById('the-canvas'),
 		ctx = canvas.getContext('2d');
-
 	/*
-	* Get page info from document, resize canvas accordingly, and render page.
-	* @param num Page number.
-	*/
+* Get page info from document, resize canvas accordingly, and render page.
+* @param num Page number.
+*/
 	function renderPage(num) {
 		pageRendering = true;
 		// Using promise to fetch the page
@@ -124,14 +100,12 @@ angular.module('twebProject1App')
 			var viewport = page.getViewport(scale);
 			canvas.height = viewport.height;
 			canvas.width = viewport.width;
-
 			// Render PDF page into canvas context
 			var renderContext = {
 				canvasContext: ctx,
 				viewport: viewport
 			};
 			var renderTask = page.render(renderContext);
-
 			// Wait for rendering to finish
 			renderTask.promise.then(function () {
 				pageRendering = false;
@@ -142,15 +116,13 @@ angular.module('twebProject1App')
 				}
 			});
 		});
-
 		// Update page counters
 		document.getElementById('page_num').textContent = pageNum;
 	}
-
 	/*
-	* If another page rendering in progress, waits until the rendering is
-	* finised. Otherwise, executes rendering immediately.
-	*/
+* If another page rendering in progress, waits until the rendering is
+* finised. Otherwise, executes rendering immediately.
+*/
 	function queueRenderPage(num) {
 		if (pageRendering) {
 			pageNumPending = num;
@@ -158,10 +130,9 @@ angular.module('twebProject1App')
 			renderPage(num);
 		}
 	}
-
 	/**
-	* Displays previous page.
-	*/
+* Displays previous page.
+*/
 	$scope.onPrevPage = function () {
 		if (pageNum <= 1) {
 			return;
@@ -169,10 +140,9 @@ angular.module('twebProject1App')
 		pageNum--;
 		queueRenderPage(pageNum);
 	}
-
 	/**
-	* Displays next page.
-	*/
+* Displays next page.
+*/
 	$scope.onNextPage = function () {
 		if (pageNum >= pdfDoc.numPages) {
 			return;
@@ -180,16 +150,13 @@ angular.module('twebProject1App')
 		pageNum++;
 		queueRenderPage(pageNum);
 	}
-
 	/**
-	* Asynchronously downloads PDF.
-	*/
+* Asynchronously downloads PDF.
+*/
 	PDFJS.getDocument(url).then(function (pdfDoc_) {
 		pdfDoc = pdfDoc_;
 		document.getElementById('page_count').textContent = pdfDoc.numPages;
 		// Initial/first page rendering
 		renderPage(pageNum);
 	});
-
 });
-
