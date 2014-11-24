b6064dbbcc260cce157334c6020459dafde6fff5
Roubaty Anthony <anthony.roubaty@gmail.com>
2014-11-24 15:00:51 +0100

    adding callback function

--- client/app/presenterSide/presenterSide.controller.js
+++ client/app/presenterSide/presenterSide.controller.js
@@ -3,6 +3,7 @@
 angular.module('twebProject1App')
   .controller('PresentersideCtrl', function ($scope, $http, socket) {
     
+	//TODO
 	//pour récupère le GET de presenterSide?presentationId=XXX
 	//$scope.presentationId = $location.search().presentationId;
 	
@@ -16,25 +17,11 @@ angular.module('twebProject1App')
   
 	$http.get('/api/messages').success(function(listeMsg) {
 		$scope.listeMsg = listeMsg;
-		socket.syncUpdates('message', $scope.listeMsg);
+		socket.syncUpdates('message', $scope.listeMsg, function(event, item, array){
+			//TODO
+			//Modification des msg
+		});
     });
-
-	$scope.send = function() {
-      if($scope.inputChat === '') {
-        return;
-      }
-	  
-	  $scope.date = new Date();
-	  $scope.formedDate = $scope.date.getHours() + 'h' + $scope.date.getMinutes() + 'm' + $scope.date.getSeconds() + 's';
-	  
-      $http.post('/api/messages', { name: $scope.inputChat , time : $scope.formedDate, type : "message"});
-      $scope.inputChat = '';
-	  
-    };
-	
-	/*$scope.$on('$destroy', function () {
-      socket.unsyncUpdates('message');
-    });	*/
 	
 	$scope.scroll = function(type){
 		document.getElementById('chatDisplay').scrollTop = 99999;
