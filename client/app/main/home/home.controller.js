fa5d7263b2e2b66e3aeb29425c92a3aabe725098
Auriana <auriana.hug@heig-vd.ch>
2014-11-24 17:29:36 +0100

    updating many things

--- client/app/main/home/home.controller.js
+++ client/app/main/home/home.controller.js
@@ -3,29 +3,6 @@
 angular.module('twebProject1App')
   .controller('HomeCtrl', function ($scope, $http, socket, Auth, $window) {
     $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
-	
-	$scope.pdfList = ["PDF1", "PDF2", "PDF3"];
 	
 	//Create lesson
 	$scope.startPresentation = function(){
