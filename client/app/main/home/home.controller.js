5b5d10202b2f57234c4b3b15b995e18a35cf6323
Roubaty Anthony <anthony.roubaty@gmail.com>
2014-11-24 15:56:45 +0100

    home creation presentation

--- client/app/main/home/home.controller.js
+++ client/app/main/home/home.controller.js
@@ -1,8 +1,7 @@
 'use strict'
 
 angular.module('twebProject1App')
-  .controller('HomeCtrl', function ($scope, $http, socket, Auth) {
-  
+  .controller('HomeCtrl', function ($scope, $http, socket, Auth, $window) {
     $scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
@@ -26,10 +25,58 @@ angular.module('twebProject1App')
       socket.unsyncUpdates('thing');
     });
 	
-	
-	
 	$scope.pdfList = ["PDF1", "PDF2", "PDF3"];
 	
+	//Create lesson
+	$scope.startPresentation = function(){
+		/*if ((typeof $scope.newLecture_title != 'undefined' ) || ($scope.newLecture_title === '')) {
+			alert('Please to give a title to this lecture');
+			return;
+		}
+		
+		if ((typeof $scope.newLecture_descr != 'undefined') || ($scope.newLecture_descr === '')) {
+			alert('Please to give a description to this lecture');
+			return;
+		}
+		
+		if ((typeof $scope.newLecture_pass != 'undefined') || ($scope.newLecture_pass === '')) {
+			alert('Please to give a password to this lecture');
+			return;
+		}*/
+		
+		alert("test");
+		
+		$http.post('/api/presentations', {
+          title: $scope.newLecture_title,
+          description: $scope.newLecture_descr,
+          pdfPath: 'notImplement',
+          userId: Auth.getCurrentUser()._id,
+          date_creation: getTime(),
+          page: 1,
+          password: $scope.newLecture_pass
+        }).success(function(data, status, headers, confi){
+			$window.location = '/presenterSide?presentationId=' + data._id;
+		});
+	};
+	
+	function getTime() {
+      var d = new Date();
+      var h = addZero(d.getHours());
+      var m = addZero(d.getMinutes());
+      var s = addZero(d.getSeconds());
+      var y = d.getFullYear();
+      var month = d.getMonth();
+      var day = d.getDate();
+      var x = y + "/" + month + "/" + day + " " + h + ":" + m + ":" + s;
+      return x;
+    }
+	
+	function addZero(i) {
+      if (i < 10) {
+        i = "0" + i;
+      }
+      return i;
+    }
 	
   });
 
