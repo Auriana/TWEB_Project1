73913944f475ab2298f840967d73d30ee17a590a
Roubaty Anthony <anthony.roubaty@gmail.com>
2015-01-05 15:14:43 +0100

    Deleting Old Thing API.

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -20,7 +20,6 @@ function onConnect(socket) {
   // Insert sockets below
   require('../api/presentation/presentation.socket').register(socket);
   require('../api/message/message.socket').register(socket);
-  require('../api/thing/thing.socket').register(socket);
 }
 
 module.exports = function (socketio) {
